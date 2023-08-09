aca713fb5935b081676d00b96eb364001277797b
Pranav S Koundinya <pranavpg101@gmail.com>
2023-08-09 12:03:22 +0530

    Fix applet page full screen size in mobile browsers to include the addressbar.

--- src/pages/applet.tsx
+++ src/pages/applet.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { type CSSProperties, type FC, useCallback, useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 import { Applet, type AppletState } from '#/features/applet';
@@ -8,11 +8,38 @@ interface LoadedData {
   id: string;
 }
 
+interface CustomStyle extends CSSProperties {
+  '--doc-height': string;
+}
+
+function getCustomStyle(height: number | undefined): CustomStyle {
+  return {
+    '--doc-height': `${height}px` ?? '100vh',
+  };
+}
+
+function useDocumentHeight() {
+  const [height, setHeight] = useState<number | undefined>();
+
+  const handleResize = useCallback(() => {
+    setHeight(window.innerHeight);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [handleResize]);
+
+  return getCustomStyle(height);
+}
+
 const AppletPage: FC = () => {
   const { initialState } = useLoaderData() as LoadedData;
+  const style = useDocumentHeight();
 
   return (
-    <div className="box-border h-screen p-1 xl:container">
+    <div className="box-border h-[_var(--doc-height)] p-1 xl:container" style={style}>
       <Applet initialState={initialState} />
     </div>
   );
