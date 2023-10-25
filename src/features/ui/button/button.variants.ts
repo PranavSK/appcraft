37f017add4ec910926488229bce83fb334e8ebb0
Pranav S Koundinya <pranavpg101@gmail.com>
2023-10-25 15:41:18 +0530

    Added cursor to CTA button.

--- src/features/ui/button/button.variants.ts
+++ src/features/ui/button/button.variants.ts
@@ -2,9 +2,9 @@ import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
   [
-    'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background',
-    'transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring',
-    'focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+    'inline-flex cursor-pointer items-center justify-center rounded-md text-sm font-medium ring-offset-background',
+    'select-none transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring',
+    'focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
   ],
   {
     variants: {
