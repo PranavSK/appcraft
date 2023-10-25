9199da8e5710565437b9f0d1d1d12ef6fc6c1cb4
Pranav S Koundinya <pranavpg101@gmail.com>
2023-10-25 18:05:04 +0530

    removed the PWA plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react-swc';
-import { VitePWA } from 'vite-plugin-pwa';
 import vConsolePlugin from 'vite-plugin-simple-vconsole';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
@@ -11,41 +10,6 @@ export default defineConfig({
     tsconfigPaths(),
     vConsolePlugin({
       enable: false,
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/s3-whjr-prod-cocos-applet.whjr.online\/app-craft/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'app-craft',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-              },
-              cacheableResponse: {
-                statuses: [200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/www.geogebra.org\/apps/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'geogebra',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-              },
-              cacheableResponse: {
-                statuses: [200],
-              },
-            },
-          },
-        ],
-      },
     }),
   ],
   base: './',
